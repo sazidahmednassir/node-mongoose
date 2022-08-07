da049a96335b6da6c7adefefc862e69bd1cc38c0
sazidahmednassir <sazidahmednassir@gmail.com>
2022-08-07 10:22:26 +0600

    defines student schema

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ mongoose.connect('mongodb://localhost:27017/students', {
 .catch(err=>
     console.error('connection falied')
 )
+
+//define student Schema
+const studentSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: String,
+    dob: Date,
+    entryDate: { type: Date, default: Date.now },
+    passed: Boolean,
+    hobbies: [String],
+    parents: {
+        father: String,
+        mother: String,
+    },
+    subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }]
+});
