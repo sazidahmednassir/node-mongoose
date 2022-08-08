272223e3473c21cb0676abae4880e675a09088a8
sazidahmednassir <sazidahmednassir@gmail.com>
2022-08-08 11:56:42 +0600

    querying data using async andalso class

--- index.js
+++ index.js
@@ -22,5 +22,67 @@ const studentSchema = new mongoose.Schema({
         father: String,
         mother: String,
     },
-    subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }]
+    subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }],
+    example:{
+        a:String,
+        type: {type:String}
+    }
 });
+
+// STUDENT Model
+const Student = mongoose.model('Student', studentSchema); // Class
+// const student = new Student({
+//     firstName: "Sazid Ahmed",
+//     lastName: "Nassir",
+//     dob: new Date("17 January 1998"),
+//     passed: true,
+//     hobbies: ["Swimming", "Singing"],
+//     parents: {
+//         father: "Nassir Ghani",
+//         mother: "Rowshan Ara Nasir",
+//     },
+//     subjects: [{ name: "Math", marks: 80 }, { name: "English", marks: 100 }],
+// });
+
+// student.save()
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err._message));
+
+//using async method
+
+// C => Create
+async function createStudent() {
+    const student = new Student({
+        firstName: "Moinul",
+        lastName: "Islam Haq",
+        dob: new Date("27 April 1995"),
+        passed: true,
+        hobbies: ["Swimming", "Singing"],
+        parents: {
+            father: "A",
+            mother: "B",
+        },
+        subjects: [{ name: "Math", marks: 80 }, { name: "English", marks: 90 }],
+    });
+
+    try {
+        const data = await student.save();
+        console.log(data);
+    } catch (err) {
+        console.log(err._message);
+    }
+}
+
+// createStudent();
+
+// R => Read
+async function readStudents() {
+    const studentsData = await Student
+        .find()
+        .limit(10)
+        .sort({ firstName: -1, lastName: 1 })
+        .select({ firstName: 1, lastName: 1, hobbies: 1 , parents:1});
+    console.log(studentsData);
+}
+
+readStudents();
